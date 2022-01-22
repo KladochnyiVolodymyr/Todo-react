9ec139dd7d0f0ed7ac4e77276b679bb435550ea4
volodymyr kladochnyi <vladimir.kladochnyi@skylum.com>
2022-01-22 18:28:19 +0200

    added clear completed functional

--- client/src/components/ControlsPanel.jsx
+++ client/src/components/ControlsPanel.jsx
@@ -1,11 +1,13 @@
-import { useAppStateContext, useUpdateItem, useFilterItems } from "../context/AppContext"
+import { useAppStateContext, useUpdateItem, useFilterItems, useDeleteItem } from "../context/AppContext"
 
 const ControlsPanel = () => {
 
     const todos = useAppStateContext().items;
-    
+
     const updateItem = useUpdateItem();
     const filterItems = useFilterItems();
+    const removeItem = useDeleteItem();
+
     const leftItems = todos.reduce((sum, current) => sum + !current.done, 0);
 
     function setTodosStatus(status) {
@@ -22,12 +24,21 @@ const ControlsPanel = () => {
         }
     }
 
+    const clearCompletedItems = () => {
+        todos.forEach(item => {
+            if(item.done) {
+                removeItem(item._id);
+            }
+        });
+    }
+
     return (
         <div>
             <button onClick={switchTodosStatus}>Switch All</button>
             <button onClick={ () => filterItems('all')}>All</button>
             <button onClick={ () => filterItems('active')}>Active</button>
             <button onClick={ () => filterItems('completed')}>Completed</button>
+            <button onClick={clearCompletedItems}>Clear Completed</button>
             <p>{leftItems} left items</p>
         </div>
     )
