09fcac42e0970890e18cadf767a42a8f1e5dfd66
volodymyr kladochnyi <vladimir.kladochnyi@skylum.com>
2022-01-23 15:29:23 +0200

    added edit title functional

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,12 +1,42 @@
+import {useState, useEffect} from "react";
 import { useDeleteItem, useUpdateItem } from "../context/AppContext"
-const Todo = ({item}) => {
 
+function useDebounce(value, delay) {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+    useEffect(
+      () => {
+        const handler = setTimeout(() => {
+          setDebouncedValue(value);
+        }, delay);
+        return () => {
+          clearTimeout(handler);
+        };
+      },
+      [value, delay]
+    );
+    return debouncedValue;
+};
+
+const Todo = ({item}) => {
+    const [value, setValue] = useState(item.title);
     const removeItem = useDeleteItem();
     const updateItem = useUpdateItem();
+    const useDebounceRes = useDebounce(value, 3000);
 
+
+    useEffect(() => {
+        if (!useDebounceRes) return
+        updateItem(item, {title: useDebounceRes});
+        // Do I need to add item and updateItem in depending?
+    }, [useDebounceRes])
+    
     return (
         <div className="todo">
-            <span>{item.title}---</span>
+            <input 
+                type="text" 
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
             <span onClick={() => removeItem(item._id)}>X</span>
             <input
                 type="checkbox"
