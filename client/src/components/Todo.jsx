0f97fb74f0cbb006e20d6474b8fef63173c81653
volodymyr kladochnyi <vladimir.kladochnyi@skylum.com>
2022-02-03 19:29:05 +0200

    added basic editing logic

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,13 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useDeleteItem, useUpdateItem } from "../context/AppContext";
 import useDebounce from "../hooks/useDebounce";
 import { toast } from "react-toastify";
 
 const Todo = ({ item }) => {
   const [value, setValue] = useState(item.title);
+  const [editing, setEditing] = useState(false);
   const removeItem = useDeleteItem();
   const updateItem = useUpdateItem();
   const useDebounceRes = useDebounce(value, 2000);
+  const inputEl = useRef(null);
 
   useEffect(() => {
     if (!useDebounceRes) return;
@@ -15,14 +17,33 @@ const Todo = ({ item }) => {
     // Do I need to add item and updateItem in depending?
   }, [useDebounceRes]);
 
+  const handleEditing = () => {
+    setEditing(true);
+    inputEl.current.focus();
+  };
+
   return (
     <div className="todo input-group mb-2">
+      <button className="btn btn-secondary" onClick={handleEditing}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-pencil"
+          viewBox="0 0 16 16"
+        >
+          <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"></path>
+        </svg>
+      </button>
       <input
         type="text"
         value={value}
         onChange={(e) => setValue(e.target.value)}
         className="form-control"
+        ref={inputEl}
       />
+      
       <div className="input-group-text">
         <input
           type="checkbox"
