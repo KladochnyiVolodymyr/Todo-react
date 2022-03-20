dd31772f40421e36732a62200d8819caab419470
volodymyr kladochnyi <vladimir.kladochnyi@skylum.com>
2022-03-20 19:28:08 +0200

    removed context

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import App from './App';
-import { AppContextProvider } from "./context/AppContext";
 import { createStore, compose, applyMiddleware } from "redux";
 import { rootReducer } from './redux/rootReducer';
 import { Provider as ReduxProvider } from 'react-redux';
@@ -19,11 +18,9 @@ const store = createStore(rootReducer, compose(
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppContextProvider>
       <ReduxProvider store={store}>
         <App />
       </ReduxProvider>
-    </AppContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
